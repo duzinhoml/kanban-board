3f517b0053ec84f7030e33a9008b544a29e46b98
Mathew Lopez <mathewlopez10@gmail.com>
2025-02-06 18:36:43 -0500

    1.0.8

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,15 +5,17 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-      // host: 'localhost',
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const dbUrl = process.env.DB_URL;
+if (!dbUrl) {
+  throw new Error('DB_URL environment variable is not defined');
+}
+
+const sequelize = new Sequelize(dbUrl, {
+  dialect: 'postgres',
+  dialectOptions: {
+    decimalNumbers: true,
+  },
+});
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);

46522f5a6e3a19235dd7522e9065c0eef74be75a
Mathew Lopez <mathewlopez10@gmail.com>
2025-02-06 19:11:37 -0500

    1.1.1

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -2,6 +2,8 @@ import { Router, Request, Response } from 'express';
 import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
